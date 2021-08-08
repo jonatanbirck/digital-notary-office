d01c749f205e40b5f12816a11984200d117fa45f
Jonatan Birck <jonatan.birck@universo.univates.br>
2021-08-08 08:07:38 -0300

    initial

---
+++ src/main/java/com/jonatan/digitalnotaryoffice/DigitalNotaryOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.jonatan.digitalnotaryoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalNotaryOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalNotaryOfficeApplication.class, args);
+	}
+
+}
