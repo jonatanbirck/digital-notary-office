f77ae7e26bebcba285478b78f55a412c29931cff
Jonatan Birck <jonatan.birck@universo.univates.br>
2021-08-08 08:25:45 -0300

    create address, certificate and notary entities

---
+++ src/main/java/com/jonatan/digitalnotaryoffice/domain/entity/Certificate.java
@@ -0,0 +1,33 @@
+package com.jonatan.digitalnotaryoffice.domain.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "certificates")
+public class Certificate {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(columnDefinition="TEXT")
+    private String description;
+
+    @ManyToMany(mappedBy = "certificates")
+    private List<Notary> notaries = new ArrayList<>();
+}
---
+++ src/main/java/com/jonatan/digitalnotaryoffice/domain/entity/Notary.java
@@ -0,0 +1,44 @@
+package com.jonatan.digitalnotaryoffice.domain.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "notaries")
+public class Notary {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "ref_address")
+    private Address address;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "notaries_certificates",
+        joinColumns = @JoinColumn(name = "ref_notary"),
+        inverseJoinColumns = @JoinColumn(name = "ref_certificate")
+    )
+    private List<Certificate> certificates = new ArrayList<>();
+
+}
