3c0fea8f76ca908599efcc0d66cbd0ca1e1c2c2d
Jonatan Birck <jonatan.birck@universo.univates.br>
2021-08-08 20:34:32 -0300

    implement certificate view and features

--- src/main/java/com/jonatan/digitalnotaryoffice/domain/entity/CertificateState.java
+++ src/main/java/com/jonatan/digitalnotaryoffice/domain/entity/CertificateState.java
@@ -1,5 +1,13 @@
 package com.jonatan.digitalnotaryoffice.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum CertificateState {
-    ACTIVE, INACTIVE
+    ACTIVE("Ativo"),
+    INACTIVE("Inativo");
+
+    private final String displayValue;
 }
